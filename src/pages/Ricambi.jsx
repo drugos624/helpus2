750e2ff0e455921aa8b90de737c7ddc4fdfe4131
drugos624 <dilucia.gabriele@gmail.com>
2025-11-02 23:01:31 +0100

    versione_7

--- src/pages/Ricambi.jsx
+++ src/pages/Ricambi.jsx
@@ -1,14 +1,21 @@
 import { useNavigate, useParams } from "react-router-dom"
 import ImageZoomable from "../components/ImageZoomable";
-
+import dati from '../impianti.json';
 
 
 function Ricambi() {
     const navigate = useNavigate();
     const { opzioni } = useParams();
 
-     const ricambiModello = dati.ricambi[opzioni]
-
+    const ricambiModello = dati.ricambi[opzioni]
+    
+    const ricambiArray = Object.entries(ricambiModello).map(([numero, contenuto]) => {
+    return {
+        numero: numero,
+        items: Array.isArray(contenuto) ? contenuto : [contenuto]
+      
+    };
+    });     
 
 
     return (
@@ -26,8 +33,33 @@ function Ricambi() {
 
             <h1>Ricambi { opzioni}</h1>
 
-             <button onClick={()=>navigate("/")}>HOME</button>
+            <button onClick={() => navigate("/")}>HOME</button><div className="lista-ricambi-container">
+    {ricambiArray.map(ricambio => (
+        <div key={ricambio.numero} className="ricambio-gruppo">
+            {/* Numero del ricambio */}
+            <h4 className="numero-ricambio">{ricambio.numero}</h4>
+            
+            {/* Tutti gli items di quel numero */}
+            <div className="ricambio-items">
+                {ricambio.items.map((item, index) => (
+                    <div key={index} className="ricambio-item">
+                        <span className="nome">
+                            {item.name || item.nome}
+                        </span>
+                        <span className="codice">
+                            {item.code}
+                        </span>
+                    </div>
+                ))}
+            </div>
         </div>
+    ))}
+</div>
+            
+
+
+        </div>
+        
         
        
     )
