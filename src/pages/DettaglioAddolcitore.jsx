508316d0306beb2dacf6c64c72bf8d364fb76463
drugos624 <dilucia.gabriele@gmail.com>
2025-11-08 22:53:39 +0100

    icone

--- src/pages/DettaglioAddolcitore.jsx
+++ src/pages/DettaglioAddolcitore.jsx
@@ -1,75 +1,64 @@
-import { useNavigate, useParams } from "react-router-dom"
-import specifiche from "../impianti.json"
+import { useNavigate, useParams } from "react-router-dom";
+import specifiche from "../impianti.json";
 
 function DettaglioAddolcitore() {
+  const navigate = useNavigate();
+  const { impianti, opzione, dimensioni } = useParams();
+  const keys = Object.keys(specifiche.impianti[impianti][opzione][dimensioni]);
+  const param = Object.entries(
+    specifiche.impianti[impianti][opzione][dimensioni]
+  );
+  const dati = specifiche.impianti[impianti][opzione][dimensioni];
 
-    const navigate = useNavigate()
-    const { impianti, opzione, dimensioni } = useParams()
-    const keys = Object.keys(specifiche.impianti[impianti]
-    [opzione][dimensioni])
-    const param =  Object.entries(specifiche.impianti[impianti]
-    [opzione][dimensioni])
-    const dati = specifiche.impianti[impianti]
-    [opzione][dimensioni]
+  //prove varie
 
-    //prove varie
+  const sottogruppi = specifiche.impianti[impianti][opzione][dimensioni];
+  const gruppi = Object.keys(sottogruppi);
 
-    const sottogruppi = specifiche.impianti[impianti]
-    [opzione][dimensioni]
-    const gruppi = Object.keys(sottogruppi)
-    
-
-    return (
-        <div className="header" >
-              <h1>Dettagli {opzione.toUpperCase()}: {dimensioni}</h1>
-              <button onClick={() => (navigate(`/ricambi/${opzione}`))}>RICAMBI { opzione}</button>
-            <div >
-               
-
-                <div>
-                    {Object.entries(sottogruppi).map(([nomeGruppo, campiGruppo]) => (
-                        <div key={nomeGruppo} className="card">
-                            <h3 >
-                                <span>
-                                    💧
-                                </span>
-                                {nomeGruppo.toUpperCase()}
-                            </h3>
-                            <div className="card-item">
-                                {Object.entries(campiGruppo).map(([chiave, valore]) => (
-                                    <div key={chiave} className="dettaglio-item">
-                                        <span className="dettaglio-chiave">{chiave.toUpperCase()}:  </span>
-                                        <span className="dettaglio-valore">{valore}</span>
-                                    </div>
-                                ))}
-
-                            </div>
-                        </div>
-                    ))}
-
-
-                </div>
-
+  return (
+    <div className="header">
+      <h1>
+        Dettagli {opzione.toUpperCase()}: {dimensioni}
+      </h1>
+      <button onClick={() => navigate(`/ricambi/${opzione}`)}>
+        RICAMBI {opzione}
+      </button>
+      <div>
+        <div>
+          {Object.entries(sottogruppi).map(([nomeGruppo, campiGruppo]) => (
+            <div key={nomeGruppo} className="card">
+              <h3>
+                <span>
+                  {nomeGruppo === "portate"
+                    ? "💦"
+                    : nomeGruppo === "capacità"
+                    ? "🔋"
+                    : nomeGruppo === "dimensioni"
+                    ? "​📐"
+                    : "⛑️"}
+                </span>
+                {nomeGruppo.toUpperCase()}
+              </h3>
+              <div className="card-item">
+                {Object.entries(campiGruppo).map(([chiave, valore]) => (
+                  <div key={chiave} className="dettaglio-item">
+                    <span className="dettaglio-chiave">
+                      {chiave.toUpperCase()}:{" "}
+                    </span>
+                    <span className="dettaglio-valore">{valore}</span>
+                  </div>
+                ))}
+              </div>
             </div>
-            <button onClick={() => (navigate(`/ricambi/${opzione}`))}>RICAMBI { opzione}</button>
-            <button onClick={() => (
-                navigate("/")
-         )}>HOME</button>
-        
+          ))}
         </div>
-
-    )
+      </div>
+      <button onClick={() => navigate(`/ricambi/${opzione}`)}>
+        RICAMBI {opzione}
+      </button>
+      <button onClick={() => navigate("/")}>HOME</button>
+    </div>
+  );
 }
-        
 
-      
- 
-
-        
-        
-        
-    
-
-
-
-export default DettaglioAddolcitore
+export default DettaglioAddolcitore;
